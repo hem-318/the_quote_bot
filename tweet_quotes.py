e4af9a8566f8dc8c6bf58366d06d4c16e073888f
Hem <66408922+icecrac34r@users.noreply.github.com>
2022-02-06 15:38:21 +0530

    Create tweet_quotes.py

---
+++ tweet_quotes.py
@@ -0,0 +1,33 @@
+#Importing required modules
+import requests
+import tweepy
+from time import sleep
+from credentials import *
+from time import sleep
+
+#Calling twitter api
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+#Where the magic happens
+n=1
+sleep_time = input("Enter the duration between 2 tweets - ") # To set a sleep time
+
+while n<1000000000000000000000000000000:
+
+    try:
+        url = 'https://api.quotable.io/random' #Website for scraping quotes
+        r = requests.get(url)
+        quote = r.json()
+        a = quote['content'] + '    -' + quote['author']
+        print('Number of tweets so far - ', n)
+        print ('Quote - ' , a)
+        tweet = api.update_status(a) #tweet the quote
+        n = n+1
+        sleep(sleep_time)
+
+    except StopIteration:
+        break
+    
+#Code by @icecracker_hem (github.com/icecrac34r)
