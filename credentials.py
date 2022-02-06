8c98c96ef6c6236e675d8feb567ee8c2d2c7b2d1
Hem <66408922+icecrac34r@users.noreply.github.com>
2022-02-06 15:36:00 +0530

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# Twitter apis
+# get'em from here - https://developer.twitter.com/en/portal/dashboard
+# Twitter application consumer keys and access tokens.
+# Remember, keep this information private.
+consumer_key = 'XX'
+consumer_secret = 'XX'
+access_token = 'XX'
+access_token_secret = 'XX'
